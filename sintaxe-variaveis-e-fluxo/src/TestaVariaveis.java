3473d90bfd7c0c3a8bd87e0a9aa78a3c9c01f5d8
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-05-25 18:39:13 -0300

    Alterações finais do dia

--- sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -2,14 +2,10 @@
 public class TestaVariaveis {
 
 	public static void main(String[] args) {
-		System.out.println("Olá novo teste");
+		int idade = 68;
+		boolean ehIdoso = idade >= 65;
 		
-		int idade;
-		idade = 37;
-		
-		System.out.println(idade);
-		
-		System.out.println("A minha idade é " + idade);
+		System.out.println(ehIdoso);
 		
 	}
 }
