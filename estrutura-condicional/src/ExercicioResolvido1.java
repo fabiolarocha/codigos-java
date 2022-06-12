03147a7efabf761eab7fa9e4373ecd528d7c14ac
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-06-12 18:00:53 -0300

    Exercicios resolvidos

---
+++ estrutura-condicional/src/ExercicioResolvido1.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioResolvido1 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1 = sc.nextDouble();
+		double nota2 = sc.nextDouble();
+
+	}
+
+}
